24d33990f1f89729e75e40dff42f00970388600f
David Ackerman <ackermand@janelia.hhmi.org>
2024-06-14 14:45:36 -0400

    perf: :zap: calculates polylines for skeletons in a better by first splitting them at branchpoints

--- src/igneous_daskified/util/skeleton_util.py
+++ src/igneous_daskified/util/skeleton_util.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from funlib.persistence import Array, open_ds
 from funlib.geometry import Roi
 import numpy as np
@@ -27,8 +28,13 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+@dataclass
+class Source:
+    vertex_attributes = []
+
+
 class CustomSkeleton:
-    def __init__(self, vertices=[], edges=[], radii=[], polylines=[]):
+    def __init__(self, vertices=[], edges=[], radii=None, polylines=[]):
         self.vertices = []
         self.edges = []
         self.radii = []
@@ -36,6 +42,9 @@ class CustomSkeleton:
 
         self.add_vertices(vertices, radii=radii)
         self.add_edges(edges)
+        if not polylines:
+            g = self.skeleton_to_graph()
+            polylines = self.get_polylines_positions_from_graph(g)
         self.add_polylines(polylines)
 
     def _get_vertex_index(self, vertex):
@@ -86,7 +95,8 @@ class CustomSkeleton:
             simplified_polyline = rdp(polyline, epsilon=tolerance_nm)
             for vertex in simplified_polyline:
                 vertices.append(tuple(vertex))
-                radii.append(self.radii[self._get_vertex_index(tuple(vertex))])
+                if self.radii:
+                    radii.append(self.radii[self._get_vertex_index(tuple(vertex))])
             simplified_polylines.append(simplified_polyline)
 
             edges.extend(list(zip(simplified_polyline, simplified_polyline[1:])))
@@ -103,7 +113,7 @@ class CustomSkeleton:
 
         for node in graph.nodes:
             degree = graph.degree[node]
-            if degree == 1:
+            if degree <= 1:
                 endpoints.append(node)
             elif degree > 2:
                 branchpoints.append(node)
@@ -112,17 +122,89 @@ class CustomSkeleton:
 
     @staticmethod
     def get_polylines_from_graph(g):
-        branchpoints, endpoints = CustomSkeleton.find_branchpoints_and_endpoints(g)
         polylines = []
-        polyline_endpoints = endpoints + branchpoints
-        for idx, polyline_endpoint_1 in enumerate(polyline_endpoints[:-1]):
-            # if path between node and branchpoint does not contain another branchpoint, then it is a polyline
-            for polyline_endpoint_2 in polyline_endpoints[idx + 1 :]:
-                path = nx.dijkstra_path(
-                    g, polyline_endpoint_1, polyline_endpoint_2, weight="weight"
+        edges = list(g.edges)
+        g_copy = g.copy()
+        branchpoints, _ = CustomSkeleton.find_branchpoints_and_endpoints(g_copy)
+        g_copy.remove_nodes_from(branchpoints)
+        if len(branchpoints) > 0:
+            for component in nx.connected_components(g_copy):
+                g_sub = g_copy.subgraph(component)
+                _, current_polyline_endpoints = (
+                    CustomSkeleton.find_branchpoints_and_endpoints(g_sub)
                 )
-                if len([node for node in path if node in polyline_endpoints]) == 2:
-                    polylines.append(path)
+                if len(current_polyline_endpoints) > 2:
+                    raise Exception(
+                        "Something went wrong, there should be at most 2 endpoints in a connected component"
+                    )
+
+                if len(g_sub.nodes) == 1:
+                    # then contains a single node
+                    polyline = list(g_sub.nodes)
+                else:
+                    polyline = nx.dijkstra_path(
+                        g_sub,
+                        current_polyline_endpoints[0],
+                        current_polyline_endpoints[1],
+                        weight="weight",
+                    )
+                # get edges with endpoints in them
+                edges_with_endpoints = [
+                    tuple(sorted(edge))
+                    for edge in edges
+                    if (
+                        edge[0] in current_polyline_endpoints
+                        or edge[1] in current_polyline_endpoints
+                    )
+                ]
+
+                prepended = False
+                appended = False
+                # sort edges by smallest node idx first
+                polyline_edges = list(zip(polyline[:-1], polyline[1:]))
+                polyline_edges = [tuple(sorted(edge)) for edge in polyline_edges]
+                for edge in edges_with_endpoints:
+                    if (
+                        not prepended
+                        and edge not in polyline_edges
+                        and polyline[0] in edge
+                    ):
+                        if polyline[0] == edge[0]:
+                            polyline.insert(0, edge[1])
+                        else:
+                            polyline.insert(0, edge[0])
+                        polyline_edges.append(edge)
+                        prepended = True
+                    if (
+                        not appended
+                        and edge not in polyline_edges
+                        and polyline[-1] in edge
+                    ):
+                        if polyline[-1] == edge[0]:
+                            polyline.append(edge[1])
+                        else:
+                            polyline.append(edge[0])
+                        polyline_edges.append(edge)
+                        appended = True
+                polylines.append(polyline)
+        else:
+            # then contains separate part(s)
+            for component in nx.connected_components(g):
+                g_sub = g.subgraph(component)
+                path = nx.eulerian_path(g_sub)
+                polyline = [node for node, _ in path]
+                polyline.append(polyline[0])
+                polylines.append(polyline)
+
+        # if two branchpoints separated by a single edge, teh above would ignore them, so add them back in
+        all_edges = []
+        for polyline in polylines:
+            all_edges.extend(list(zip(polyline[:-1], polyline[1:])))
+        all_edges = [tuple(sorted(edge)) for edge in all_edges]
+        (polylines)
+        for edge in g.edges:
+            if edge not in all_edges:
+                polylines.append([edge[0], edge[1]])
         return polylines
 
     @staticmethod
@@ -145,21 +227,20 @@ class CustomSkeleton:
                 ):
                     current_min_tick_length_nm = path_length_nm
                     current_min_tick_path = path
-
         if current_min_tick_path:
             g.remove_edges_from(
                 list(zip(current_min_tick_path[:-1], current_min_tick_path[1:]))
             )
             g.remove_nodes_from(list(nx.isolates(g)))
-
         return current_min_tick_path, g
 
     def skeleton_to_graph(self):
         g = nx.Graph()
         g.add_nodes_from(range(len(self.vertices)))
         # add radii as properties to the nodes
-        for idx in range(len(self.vertices)):
-            g.nodes[idx]["radius"] = self.radii[idx]
+        if self.radii:
+            for idx in range(len(self.vertices)):
+                g.nodes[idx]["radius"] = self.radii[idx]
         g.add_edges_from(self.edges)
         # add edge weights to the graph where weights are the distances between vertices
         for edge in self.edges:
@@ -169,13 +250,7 @@ class CustomSkeleton:
 
         return g
 
-    def graph_to_skeleton(self, g):
-        vertices = [self.vertices[idx] for idx in g.nodes]
-        radii = [self.radii[idx] for idx in g.nodes]
-        edges = fastremap.remap(
-            np.array(g.edges), dict(zip(list(g.nodes), list(range(len(g.nodes)))))
-        )
-        edges = edges.tolist()
+    def get_polylines_positions_from_graph(self, g):
         polylines = []
         for polyline_by_vertex_id in CustomSkeleton.get_polylines_from_graph(g):
             polylines.append(
@@ -186,17 +261,36 @@ class CustomSkeleton:
                     ]
                 )
             )
+        return polylines
+
+    def graph_to_skeleton(self, g):
+        vertices = [self.vertices[idx] for idx in g.nodes]
+        radii = [self.radii[idx] for idx in g.nodes]
+        edges = fastremap.remap(
+            np.array(g.edges), dict(zip(list(g.nodes), list(range(len(g.nodes)))))
+        )
+        edges = edges.tolist()
+        polylines = self.get_polylines_positions_from_graph(g)
         return CustomSkeleton(vertices, edges, radii, polylines)
 
-    def prune(self, min_tick_length_nm=200):
+    def prune(self, min_branch_length_nm=200):
 
         g = self.skeleton_to_graph()
         current_min_tick_path, g = CustomSkeleton.remove_smallest_qualifying_branch(
-            g, min_tick_length_nm
+            g, min_branch_length_nm
         )
         while current_min_tick_path:
             current_min_tick_path, g = CustomSkeleton.remove_smallest_qualifying_branch(
-                g, min_tick_length_nm
+                g, min_branch_length_nm
             )
 
         return self.graph_to_skeleton(g)
+
+    def write_neuroglancer_skeleton(self, path):
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, "wb") as f:
+            skel = NeuroglancerSkeleton(
+                self.vertices, self.edges, vertex_attributes=None
+            )
+            encoded = skel.encode(Source())
+            f.write(encoded)
